a91fda03b54cdb511d35b21b5c1be36e7fa36b55
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-01-28 13:25:31 +0900

    メモ機能バックエンド実装　DB生成

---
+++ db/migrate/20210128035000_create_memos.rb
@@ -0,0 +1,10 @@
+class CreateMemos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :memos do |t|
+      t.string :text,     null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
