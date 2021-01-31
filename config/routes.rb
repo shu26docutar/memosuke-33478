c3fb113554e046063b5ab86a80e1749fe6b6da05
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-01-31 11:48:14 +0900

    ルーテング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "memos#index"
+  resources :rooms, only: [:new, :create] do
+    resources :memos, only: [:index, :create]
+  end
 end
